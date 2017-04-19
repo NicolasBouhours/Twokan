4b13a65e08ada47b3731aea8256f8bac884cdf70
Suliac <s.blineau@iia-laval.fr>
2017-04-19 16:20:24 +0200

    First commit

---
+++ Twokan/Twokan.Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twokan.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Twokan : client");
+            Console.ReadLine();
+        }
+    }
+}
