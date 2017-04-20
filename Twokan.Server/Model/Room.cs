fc47eba3af2dd5c2b7a40f2e2acefce77d9eb47e
Unknown <n.bouhours@iia-laval.fr>
2017-04-20 14:35:54 +0200

    Fix

--- Twokan.Server/Model/Room.cs
+++ Twokan.Server/Model/Room.cs
@@ -4,16 +4,16 @@
 
     public class Room
     {
-        public int Id { get; set; }
-
-        public ICollection<Question> ListQuestions { get;  }
-
-        public ICollection<Gamer> ListGamers { get; }
-
         public Room()
         {
             this.ListGamers = new List<Gamer>();
             this.ListQuestions = new List<Question>();
         }
+
+        public int Id { get; set; }
+
+        public ICollection<Question> ListQuestions { get; }
+
+        public ICollection<Gamer> ListGamers { get; }
     }
 }
