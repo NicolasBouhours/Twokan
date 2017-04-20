7f4fb195952c137b48f98e1dcb6f31343d096d35
floran50 <floran50@hotmail.fr>
2017-04-20 12:20:42 +0200

    Fix Code And Remove Test

--- Twokan.Client/Display/Question.cs
+++ Twokan.Client/Display/Question.cs
@@ -8,7 +8,8 @@
         /// Ecran des questions 
         /// </summary>
         /// <param name="code">numéro de la partie</param>
-        //[TODO] Recupèrer les question sur le serveur
+        
+        ////[TODO] Recupèrer les question sur le serveur
         public Question(string code)
         {
             string saisie = string.Empty;
@@ -18,15 +19,16 @@
             Console.WriteLine("Appuyer sur une touche pour continuer");
             saisie = Console.ReadLine();
 
-            //Regarde si le joueur souhaite quitter la partie et le fait quitter
-            if(CheckIfHeWantLeave(saisie))
+            ////Regarde si le joueur souhaite quitter la partie et le fait quitter
+            if (CheckIfHeWantLeave(saisie))
             {
                 QuitGame();
             }
 
 
-            //[TODO] Recupére les questions du jeux sur le serveur et commence le Jeux
-            //throw new NotImplementedException("Aucune question sur le serveur");
+            ////[TODO] Recupére les questions du jeux sur le serveur et commence le Jeux
+
+            ////throw new NotImplementedException("Aucune question sur le serveur");
         }
 
 
@@ -37,13 +39,13 @@
         /// <returns></returns>
         public Boolean CheckIfHeWantLeave(string saisie)
         {
-            //Permet de retourner au menu
+            ////Permet de retourner au menu
             if (saisie == "q")
             {
                 Console.WriteLine("Etes-vous sûr de vouloir quitter ? (Y/N)");
                 saisie = Console.ReadLine();
 
-                //Regarde si il souhaite vraiment quitter
+                ////Regarde si il souhaite vraiment quitter
                 if (saisie == "Y")
                 {
                     return true;
@@ -62,12 +64,16 @@
         /// <summary>
         /// Retourne au Main Menu
         /// </summary>
-        // [TODO] Retourner sur le main menu
+        
+        //// [TODO] Retourner sur le main menu
         public void QuitGame()
         {
-            //[TODO]
-            //MainMenu main = new MainMenu();
-            //throw new NotImplementedException("Main menu not create");
+
+            ////[TODO]
+
+            ////MainMenu main = new MainMenu();
+
+            ////throw new NotImplementedException("Main menu not create");
         }
     }
 }
--- Twokan.ServerTests/TestQuestion.cs
+++ Twokan.ServerTests/TestQuestion.cs
@@ -1,22 +1,9 @@
 ﻿namespace Twokan.ServerTests
 {
     using NUnit.Framework;
-    using Twokan.Client.Display;
 
     [TestFixture]
     public class TestQuestion
     {
-        /// <summary>
-        /// Impossible de réaliser le test sans MOC
-        /// </summary>
-        [Test]
-        public void TestCheckIfHeWantLeaveTrue()
-        {
-            new Question("8464");
-            //bool result = q.checkIfHeWantLeave("q");
-            bool result = true;
-            Assert.AreEqual(true, result);
-
-        }
     }
 }
