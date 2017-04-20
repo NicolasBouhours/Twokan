798d73e47d4e10ae391b0c6ddd2d300345a08cbc
Nato <tom.vaidie@gmail.com>
2017-04-20 11:37:35 +0200

    Fusion suite

--- Twokan.Client/Program.cs
+++ Twokan.Client/Program.cs
@@ -5,13 +5,39 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using Display;
 
     public static class Program
     {
+        #region Fields
+
+        static MainMenu screenMainMenu;
+        
+        #endregion
+
         private static void Main()
         {
             Console.WriteLine("Twokan : client");
+            ScreenMainMenu = new MainMenu();
+
             Console.ReadLine();
         }
+
+        #region Properties
+        
+        internal static MainMenu ScreenMainMenu
+        {
+            get
+            {
+                return screenMainMenu;
+            }
+
+            set
+            {
+                screenMainMenu = value;
+            }
+        }
+
+        #endregion
     }
 }
